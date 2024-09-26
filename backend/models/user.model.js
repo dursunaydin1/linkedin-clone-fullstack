c27e52c3c9c44b120caadb2cca8b8b929789d892
dursunaydin <dursunayydin@gmail.com>
2024-09-26 20:57:14 +0300

    Sing up Route

---
+++ backend/models/user.model.js
@@ -0,0 +1,68 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilePicture: {
+      type: String,
+      default: "",
+    },
+    bannerImg: {
+      type: String,
+      default: "",
+    },
+    headline: {
+      type: String,
+      default: "Linkedin User",
+    },
+    location: {
+      type: String,
+      default: "Earth",
+    },
+    about: {
+      type: String,
+      default: "",
+    },
+    skills: [String],
+    experience: [
+      {
+        title: { type: String },
+        company: { type: String },
+        startDate: { type: Date },
+        endDate: { type: Date },
+        description: { type: String },
+      },
+    ],
+    education: [
+      {
+        school: { type: String },
+        fieldofstudy: { type: String },
+        startYear: { type: Number },
+        endYear: { type: Number },
+      },
+    ],
+    connections: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
