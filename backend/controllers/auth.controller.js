9362d0d8aa0ed39ffc68ed6c045c4e26eb8e7e72
dursunaydin <dursunayydin@gmail.com>
2024-09-30 17:57:44 +0300

    Login and Logout Routes

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -57,7 +57,7 @@ export const signup = async (req, res) => {
     });
 
     // JWT'yi cookie'ye yerleştir
-    res.cookie("token", token, {
+    res.cookie("jwt-linkedin", token, {
       httpOnly: true, // Cookie'yi sadece sunucuya gönder (XSS ataklarına karşı)
       maxAge: 1000 * 60 * 60 * 24 * 3, // 3 gün boyunca geçerli
       sameSite: "strict", // Cookie sadece aynı siteden gelen isteklerle gönderilir
@@ -82,11 +82,41 @@ export const signup = async (req, res) => {
 };
 
 // Giriş fonksiyonu
-export const login = (req, res) => {
-  res.send("login");
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    // Check if username and password are provided
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid username or password" });
+    }
+
+    // Check if password is correct
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid username or password" });
+    }
+
+    // Create and send a JWT token
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "3d",
+    });
+    res.cookie("jwt-linkedin", token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 3,
+      sameSite: "strict",
+      secure: process.env.NODE_ENV === "production",
+    });
+    res.status(200).json({ message: "User logged in successfully" });
+  } catch (error) {
+    console.error("Error in login:", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
 
 // Çıkış fonksiyonu
 export const logout = (req, res) => {
-  res.send("logout");
+  res.clearCookie("jwt-linkedin");
+  res.json({ message: "User logged out successfully" });
 };
