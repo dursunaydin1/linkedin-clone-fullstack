a86228a17785e1f5ea349fd2384a0f921bff041b
dursunaydin <dursunayydin@gmail.com>
2024-10-09 11:40:16 +0300

    Profile Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import { axiosInstance } from "./lib/axios";
 import NotificationsPage from "./pages/NotificationsPage";
 import NetworkPage from "./pages/NetworkPage";
 import PostPage from "./pages/PostPage";
+import ProfilePage from "./pages/ProfilePage";
 
 function App() {
   const { data: authUser, isLoading } = useQuery({
@@ -55,6 +56,10 @@ function App() {
         <Route
           path="/post/:postId"
           element={authUser ? <PostPage /> : <Navigate to={"/login"} />}
+        />
+        <Route
+          path="/profile/:username"
+          element={authUser ? <ProfilePage /> : <Navigate to={"/login"} />}
         />
       </Routes>
       <Toaster />
